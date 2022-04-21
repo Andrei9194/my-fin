9af748c379883e17c6a4822db1373eef9620acec
Andrei Prakapovich <andre1121311@gmail.com>
2022-04-21 21:46:10 +0200

    Create component mainpage

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom"
+import { MainPage } from "../mainPage"
 import { Modals } from "../modals"
 
 export const Content = () =>{
     return(
         <div style={{width: '100%', height: '100vh'}}>
             <Routes>
-                <Route exact path="/" element={<Modals/>} />
+                <Route exact path="/" element={<MainPage/>} />
             </Routes>
         </div>
     )
---
+++ src/components/mainPage/index.js
@@ -0,0 +1,13 @@
+import { Modals } from "../modals"
+
+export const MainPage = () =>{
+
+
+    return(
+        <div>
+            <h1>Main page</h1>
+            <Modals/>
+        </div>
+
+    )
+}
